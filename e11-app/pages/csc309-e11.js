c2313e0a3ca2625c7004c7646043341855860b02
duahuss <duahussainn@gmail.com>
2024-11-28 12:11:43 -0500

    a

---
+++ e11-app/pages/csc309-e11.js
@@ -0,0 +1,10 @@
+// pages/csc309-e11.js
+export default function AutoTester() {
+    return (
+      <div id="auto-tester">
+        Hi. I am hussa982, and I have learned how to develop a full-stack web app
+        and deploy it
+      </div>
+    );
+  }
+  
