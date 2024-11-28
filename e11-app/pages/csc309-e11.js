bcb64f522f22d4d5823a665c1b57676d8e72a9c6
duahuss <duahussainn@gmail.com>
2024-11-28 12:22:08 -0500

    a

--- e11-app/pages/csc309-e11.js
+++ e11-app/pages/csc309-e11.js
@@ -3,7 +3,7 @@ export default function AutoTester() {
     return (
       <div id="auto-tester">
         Hi. I am hussa982, and I have learned how to develop a full-stack web app
-        and deploy it
+        and deploy it!
       </div>
     );
   }
---
+++ e11-app/scripts.js
@@ -0,0 +1,14 @@
+function getBaseUrl() {
+    // return the base URL of your deployed app (only the protocol + domain)
+    return "https://e11-3.vercel.app"
+  }
+  
+  function getUTorId() {
+    // return your UTorId
+    return "hussa982"
+  }
+  
+  module.exports = {
+    getBaseUrl,
+    getUTorId,
+  };
